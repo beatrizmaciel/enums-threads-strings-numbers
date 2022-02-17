2d16b38eb41a29c0a223e53d9fcf02c0f4ff2f40
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-02-16 23:22:38 -0300

    strings - StringTokenizer

---
+++ src/com/bibs/cursojava/aula75_84/Aula84.java
@@ -0,0 +1,21 @@
+package com.bibs.cursojava.aula75_84;
+
+import java.util.StringTokenizer;
+
+public class Aula84 {
+
+	public static void main(String[] args) {
+		
+		String doArquivo = "1;Antônio;30";
+		
+		StringTokenizer st = new StringTokenizer(doArquivo, ";");
+		
+		while(st.hasMoreTokens()) {
+			System.out.println(st.nextToken());
+		}
+	}
+}
+
+/**
+ * O StringTokenizer ajuda muito na leitura de arquivos com Java.
+ */
